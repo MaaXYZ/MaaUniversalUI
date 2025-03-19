ded86aedaf4c832b70e5940f940bf10621c9c9a7
MistEO <mistereo@hotmail.com>
2025-03-19 18:28:03 +0800

    feat: add some models

--- MUU/Models/ConfigurationDataModel.cs
+++ MUU/Models/ConfigurationDataModel.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.Json.Serialization;
 
 namespace MUU.ConfModels;
 
 public class Configuration
+{
+    public required Dictionary<string, TaskConfig> allTask { get; set; }
+    public required Dictionary<string, TimerConfig> timer { get; set; }
+}
+
+public class TimerConfig
+{
+    public required List<DayOfWeek> days { get; set; }
+    public required List<int> hours { get; set; }
+
+    public required List<string> tasks { get; set; }
+}
+
+public class TaskConfig
 {
     public required List<Task> task { get; set; }
 }
